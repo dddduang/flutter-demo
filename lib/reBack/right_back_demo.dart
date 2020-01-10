c0395c981b5acd1f2088ac169659ddb86be4c0e1
章丹 <zhangdan@lqwydeiMac.local>
2020-01-10 14:15:54 +0800

    123

--- lib/reBack/right_back_demo.dart
+++ lib/reBack/right_back_demo.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/cupertino.dart'; // Flutter有两套UI模板，一套是material,另一套就是Cupertino
+// Cupertino主要针对的的就是IOS系统的UI，所以用的右滑返回上一级就是在这个Cupertino里。
 class RightBackDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
+      child: Center( // 这个和以前使用material的Scaffold类似，不过他里边的参数是child
         child: Container(
           height: 100.0,
           width: 100.0,
-          color: CupertinoColors.activeGreen,
+          color: CupertinoColors.activeGreen, // 在Cupertino下也有很多Widget控件，他们都是以Cupertino开头的，这就让我们很好区分，当然两种皮肤是可以进行混用的
           child: CupertinoButton(
             child: Icon(CupertinoIcons.add),
             onPressed: () {
@@ -24,3 +24,5 @@ class RightBackDemo extends StatelessWidget {
     );
   }
 }
+
+// 其实只要使用CupertinoPageRoute打开的子页面，就可以实现右滑返回上一级。
